c486afeab3fef7f94ba5892e2fa3134989619a21
Mira Scarvalone <email": null>
2016-01-08 18:11:12 -0500

    Update rspec tests

--- spec/flashcard_deck_spec.rb
+++ spec/flashcard_deck_spec.rb
@@ -1,11 +1,37 @@
 require_relative '../flashcard_deck'
-# require_relative '../card'
 
 describe FlashCardDeck do
-  let(:flashcard_deck) {FlashCardDeck.new}
+  let(:flashcard_deck) {FlashCardDeck.new('short.txt')}
 
-  it 'creates an empty deck' do
-    expect(flashcard_deck.deck).to eq []
+  describe 'initialize' do
+    it 'creates a deck with card elements' do
+      expect(flashcard_deck.deck[0]).to be_kind_of Card
+    end
+  end
+
+  describe '.select_card' do
+    it 'selects a random card' do
+      expect(flashcard_deck.select_card).to be_kind_of Card
+    end
+
+    it 'removes card from deck' do
+      selected_card = flashcard_deck.select_card
+      expect(flashcard_deck.deck.find_index(selected_card)).to be nil
+    end
+  end
+
+  describe '.remove_card' do
+    it 'removes card' do
+      remove_card = flashcard_deck.deck[0]
+      new_deck = flashcard_deck.remove_card(flashcard_deck.deck[0])
+      expect(flashcard_deck.deck.find_index(remove_card)).to be nil
+    end
+  end
+
+  describe '.empty?' do
+    it 'checks if array is empty' do
+      expect(flashcard_deck.empty?).to eq false
+    end
   end
 
 end
--- spec/view_spec.rb
+++ spec/view_spec.rb
@@ -3,9 +3,34 @@ require_relative '../view'
 describe View do
   let(:view) {View.new}
 
-  it 'prints message' do
-    expect { view.display("test") }.to output("test\n").to_stdout
+  describe '.intro' do
+    it 'prints intro' do
+      expect{view.intro}.to output("Welcome to Ruby Flash Cards. To play, just enter the correct term for each definition. To exit, enter exit. Ready?  Go!\n\n").to_stdout
+    end
   end
 
-# Create possible tests for input
+  describe '.display' do
+    it 'prints message' do
+      expect{view.display("test")}.to output("test\n").to_stdout
+    end
+  end
+
+  describe '.wrong_guess' do
+    it 'prints intro' do
+      expect{view.wrong_guess}.to output("That is incorrect, please try again.\n\n").to_stdout
+    end
+  end
+
+  describe '.right_guess' do
+    it 'prints right_guess' do
+      expect{view.right_guess}.to output("Correct! Next question...\n\n").to_stdout
+    end
+  end
+
+  describe '.goodbye' do
+    it 'prints goodbye' do
+      expect{view.goodbye}.to output("Thank you for studying!\n").to_stdout
+    end
+  end
+
 end
